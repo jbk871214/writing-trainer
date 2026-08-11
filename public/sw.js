d8311ae6284470a28ab465b37b1e35b41a5e7e1e
jbk871214 <317898471@qq.com>
2026-08-11 10:59:27 +0800

    初次提交

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'writing-trainer-v1';
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/']))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
